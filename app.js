2fbc9665c6cd9cadfc46ca150872ef900f27c1a9
Abhishek Batra <abhishekbatra1993@gmail.com>
2016-02-25 00:34:18 +0530

    Updated new message response

--- app.js
+++ app.js
@@ -84,6 +84,8 @@ function sendNewMessage(data) {
     requestify.post(url, data)
         .then(function (response) {
             var jsonBody = response.getBody();
+            jsonBody.imageUrl = jsonBody.image;
+            delete jsonBody.image;
             delete jsonBody.chat.messages;
             io.to(jsonBody.socketId).emit('new_message', jsonBody);
         });
