37688bfed96682aa7a8c4698ff322ee8bb33d926
Abhishek Batra <abhishekbatra1993@gmail.com>
2016-02-26 17:13:47 +0530

    Typo Fix

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -9,16 +9,14 @@ module.exports = {
 
     deleteMessages: function (req, res) {
 
-        var ids = JSON.parse(req.param(ids));q
+        var ids = JSON.parse(req.param(ids));
         Message.destroy({id: ids}).exec(function (error) {
-
             if (error) {
                 return res.json(error)
             } else {
                 return res.ok();
             }
         });
-
     }
 };
 
